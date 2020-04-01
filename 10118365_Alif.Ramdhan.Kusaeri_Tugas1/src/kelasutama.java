6c3dfc746feed8867e3fe13b9183ed4d9a068ad4
alif ramdhan kusaeri <47347128+aliframdhan@users.noreply.github.com>
2020-04-01 09:06:28 +0700

    Upload File

---
+++ 10118365_Alif.Ramdhan.Kusaeri_Tugas1/src/kelasutama.java
@@ -0,0 +1,18 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        jutama jutama = new jutama();
+        jutama.setVisible(true);
+        
+    }
+}
